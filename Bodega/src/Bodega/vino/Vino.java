ea5fae45fb4a128360c1ace3374929c43e56fc8e
Kilxz <tomasrando@gmail.com>
2023-09-02 16:22:42 -0300

    Implement Vino class
    Se implementa la clase abstracta vino

--- Bodega/src/Bodega/vino/Vino.java
+++ Bodega/src/Bodega/vino/Vino.java
@@ -1,4 +1,48 @@
 package Bodega.vino;
 
-public class Vino {
+public abstract class Vino {
+    //Declaración de variables
+    protected String nombreDeVino; //Nombre de vino se refiere a la "categoría", es decir Malbec, Sauvignon, etc
+    protected String tipoDeVino; // Referida al tipo (tinto, blanco, cava o rosa)
+    protected int lote;
+    protected int añoElaboracion; //Año en el que es insertado en el sistema
+    protected String etapaActual; //Etapa en la que se encuentra
+
+    protected float temperaturaFermentacion;
+
+    protected int tiempoCrianza;
+
+    protected int tiempoMaceracion;
+
+    //Constructor
+    public void Vino(int lote, int añoElaboracion, String etapaActual) {
+        this.lote = lote;
+        this.añoElaboracion = añoElaboracion;
+        this.etapaActual = etapaActual;
+    }
+
+    //Getters
+    //Retorna el nombre del vino
+    public String getNombreDeVino() {return nombreDeVino;}
+    //Retorna el tipo de vino
+    public String getTipoDeVino() {return tipoDeVino;}
+    //Retorna la etapa actual
+    public String getEtapaActual() {return etapaActual;}
+    //Retorna el lote
+    public int getLote() {return lote;}
+    //Retorna el año de elaboración
+    public int getAñoElaboracion() {return añoElaboracion;}
+    //Retorna el tiempo de maceración
+    public int getTiempoMaceracion() {return tiempoMaceracion;}
+    //Retorna el tiempo de crianza necesario
+    public int getTiempoCrianza() {return tiempoCrianza;}
+    //Retorna la temperatura de la fermentación
+    public float getTemperaturaFermentacion() {return temperaturaFermentacion;}
+
+    //Setters
+    public void setEtapa(String newEtapa) {etapaActual = newEtapa;}
+
+    //Servicios
+    public abstract void mostrarEtapas(); //Muestra todas las etapas por las que tiene que pasar el vino. Varía entre todos.
+
 }
